ce3973c2f6fda14e6fa1ffd8a1720bd82f6fa005
MichaelWinsauer <46480126+MichaelWinsauer@users.noreply.github.com>
2019-05-14 08:40:07 +0200

    Testenvironment Updated

--- CSharp_Unity_Projekt/Assets/Scripts/CheckGround.cs
+++ CSharp_Unity_Projekt/Assets/Scripts/CheckGround.cs
@@ -4,22 +4,17 @@ using UnityEngine;
 
 public class CheckGround : MonoBehaviour
 {
-    private GameObject player;
-    void Start()
-    {
-        player = transform.parent.gameObject;
-    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider)
         {
-            player.GetComponent<Movement>().isGrounded = true;
+            this.GetComponent<Movement>().isGrounded = true;
         }
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        player.GetComponent<Movement>().isGrounded = false;
+        this.GetComponent<Movement>().isGrounded = false;
     }
 }
