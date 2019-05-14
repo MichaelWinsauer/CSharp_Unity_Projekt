8463a29ecf16801d051bb6b53564347f52f7f986
MichaelWinsauer <46480126+MichaelWinsauer@users.noreply.github.com>
2019-05-14 07:35:13 +0200

    Testumgebung

---
+++ CSharp_Unity_Projekt/Assets/Scripts/CheckGround.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGround : MonoBehaviour
+{
+    private GameObject player;
+    void Start()
+    {
+        player = transform.parent.gameObject;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider)
+        {
+            player.GetComponent<Movement>().isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        player.GetComponent<Movement>().isGrounded = false;
+    }
+}
---
+++ CSharp_Unity_Projekt/Assets/Scripts/Movement.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public int jumpCountInput;
+    public float jumpForce;
+    public bool isGrounded = false;
+
+    private int jumpCount;
+    private Rigidbody2D rb;
+    private float moveX;
+    private float airForce = 5;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        jumpCount = jumpCountInput;
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveX = Input.GetAxis("Horizontal");
+
+    }
+
+    private void FixedUpdate()
+    {
+        move();
+
+        jump();
+    }
+
+    private void move()
+    {
+        
+        if (isGrounded)
+            rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
+        else
+        {
+            rb.velocity = new Vector2(moveX * moveSpeed / 2, rb.velocity.y);
+            //rb.AddForce(new Vector2(moveX * airForce * rb.velocity.x, 0));
+        }
+            
+    }
+
+    private void jump()
+    {
+        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.W)) && jumpCount > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            jumpCount--;
+        }
+        else if (isGrounded)
+            jumpCount = jumpCountInput;
+    }
+}
