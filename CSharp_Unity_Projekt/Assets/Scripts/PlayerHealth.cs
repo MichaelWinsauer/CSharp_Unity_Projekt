e5a1a74a2586e90972f3f433953da2c1864f894e
MichaelWinsauer <46480126+MichaelWinsauer@users.noreply.github.com>
2019-05-15 08:21:26 +0200

    Einfaches Health System implementiert

---
+++ CSharp_Unity_Projekt/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+
+    private int currentHealth;
+    private bool isDamaged = false;
+    private Movement movement;
+    private bool isDead = false;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        movement = GetComponent<Movement>();
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        isDamaged = true;
+        currentHealth -= damageAmount;
+
+        if(currentHealth <= 0 && !isDead)
+        {
+            die();
+        }
+    }
+
+    private void die()
+    {
+        isDead = true;
+        //Animation & Sound spielen. Movement und Fähigkeiten disablen
+        
+    }
+}
