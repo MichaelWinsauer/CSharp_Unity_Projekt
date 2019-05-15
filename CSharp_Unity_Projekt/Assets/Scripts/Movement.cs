aa218417fc1b188981bf9177c32d833c2e952008
MichaelWinsauer <46480126+MichaelWinsauer@users.noreply.github.com>
2019-05-15 07:57:28 +0200

    Movement fixed

--- CSharp_Unity_Projekt/Assets/Scripts/Movement.cs
+++ CSharp_Unity_Projekt/Assets/Scripts/Movement.cs
@@ -10,12 +10,15 @@ public class Movement : MonoBehaviour
     public int jumpCountInput;
     public float jumpForce;
     public bool isGrounded = false;
+    public float moveSmooth = 0.05f;
+    public float airMoveSmooth = 0.2f;
 
     private int jumpCount;
     private Rigidbody2D rb;
     private float moveX;
-    private float airForce = 2;
-    
+    private float airForce = 1.5f;
+    private Vector3 velocity = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -41,20 +44,23 @@ public class Movement : MonoBehaviour
     {
         
         if (isGrounded)
-            rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
+        {
+            Vector3 targetVelocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
+            rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, moveSmooth);
+        }
         else
         {
-            rb.velocity = new Vector2(moveX * moveSpeed / airForce, rb.velocity.y);
-            //rb.AddForce(new Vector2(moveX * moveSpeed, 0));
+            Vector3 targetVelocity = new Vector2(moveX * moveSpeed / airForce, rb.velocity.y);
+            rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, airMoveSmooth);
         }
-            
+
     }
 
     private void jump()
     {
-        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.W)) && jumpCount > 1) //Keine Ahnung, warum ich hier grade 1 machen muss, damit ich 2 Sprünge habe...
+        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)) && jumpCount > 1) //Keine Ahnung, warum ich hier grade 1 machen muss, damit ich 2 Sprünge habe...
         {
-            rb.velocity = Vector2.up * jumpForce;
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             jumpCount--;
         }
         else if (isGrounded)
