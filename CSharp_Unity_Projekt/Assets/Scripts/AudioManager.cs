8dd34e38d0344b8c7075a48735ddeb392cc0685b
Schnuppii <35272173+Schnuppii@users.noreply.github.com>
2019-05-22 10:40:10 +0200

    AudioManager hinzugefügt

---
+++ CSharp_Unity_Projekt/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    private void Awake()
+    {
+        foreach(Sound sound in sounds)
+        {
+            sound.source = gameObject.AddComponent<AudioSource>();
+            sound.source.clip = sound.clip;
+
+            sound.source.volume = sound.volume;
+            sound.source.loop = sound.loop;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if(s == null)
+        {
+            return;
+            Debug.Log("Ja des konnte ich nicht finden, mach mal was");
+        }
+        s.source.Play();
+    }
+}
