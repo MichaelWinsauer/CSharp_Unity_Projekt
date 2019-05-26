4e232145a060c568d5728803b3aa8a525367307e
MichaelWinsauer <46480126+MichaelWinsauer@users.noreply.github.com>
2019-05-26 16:20:17 +0200

    Double-Jump hinzugefügt

--- CSharp_Unity_Projekt/Assets/Scripts/CheckGround.cs
+++ CSharp_Unity_Projekt/Assets/Scripts/CheckGround.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class CheckGround : MonoBehaviour
 {
     private GameObject player;
+    private float height;
 
     void Start()
     {
         player = transform.parent.gameObject;
+        height = 50f;
     }
     
     public bool checkRaycastDistance()
     {
-        if (Physics2D.Raycast(transform.position, Vector2.down, 15f, LayerMask.NameToLayer("Ground")).distance == 0 && Physics2D.Raycast(transform.position, Vector2.down, 15f, LayerMask.NameToLayer("Ground")).collider.tag.Equals("Ground"))
+        if (Physics2D.Raycast(transform.position, Vector2.down, height, LayerMask.NameToLayer("Ground")).distance == 0 && Physics2D.Raycast(transform.position, Vector2.down, height, LayerMask.NameToLayer("Ground")).collider.tag.Equals("Ground"))
             return true;
         else
             return false;
---
+++ CSharp_Unity_Projekt/Assets/Scripts/PlayerDoubleJump.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDoubleJump : MonoBehaviour
+{
+    [SerializeField]
+    private float cooldownInput;
+    [SerializeField]
+    private float durationInput;
+    [SerializeField]
+    private float jumpForce;
+
+    private GameObject player;
+    private Movement playerMovement;
+    private Rigidbody2D playerRb;
+    private float cooldown;
+    private float duration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerMovement = player.GetComponent<Movement>();
+        playerRb = player.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cooldown -= Time.deltaTime;
+        if(cooldown <= 0)
+            if(!playerMovement.IsGrounded)
+                if(Input.GetKeyDown(KeyCode.W))
+                {
+                    cooldown = cooldownInput;
+                    duration = durationInput;
+                }
+
+        doubleJump();
+    }
+
+    private void doubleJump()
+    {
+        
+        if(duration >= 0)
+        {
+            duration -= Time.deltaTime;
+            playerRb.velocity = new Vector2(playerRb.velocity.x, jumpForce);
+        }
+        else
+        {
+        }
+    }
+}
