281bc6daf1e5e83beba56b11664b23741dd2eea6
MichaelWinsauer <46480126+MichaelWinsauer@users.noreply.github.com>
2019-06-23 12:40:21 +0200

    Menü funktioniert jetzt

--- CSharp_Unity_Projekt/Assets/Scripts/OptionButton.cs
+++ CSharp_Unity_Projekt/Assets/Scripts/OptionButton.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionButton : MonoBehaviour
 {
@@ -20,38 +21,57 @@ public class OptionButton : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (optionsButtonController.Index == index)
+        if(!isSlider)
         {
-            if (!isSlider)
+            if (optionsButtonController.Index == index)
             {
                 anim.SetBool("isSelected", true);
                 if (Input.GetButton("Submit") && !pressed)
                 {
-                    anim.SetTrigger("pressed");
                     timer = .1f;
                     pressed = true;
                 }
             }
+            else
+            {
+                anim.SetBool("isSelected", false);
+            }
         }
         else
         {
-            anim.SetBool("isSelected", false);
+            if(optionsButtonController.Index == index)
+            {
+                GetComponent<Image>().color = new Color(255, 134, 0, 255);
+                if (Input.GetAxis("Horizontal") != 0)
+                {
+                    if ((GameData.options.Volume += Input.GetAxis("Horizontal") / 100) > 1)
+                        GameData.options.Volume = 1;
+                    else if ((GameData.options.Volume += Input.GetAxis("Horizontal") / 100) < 0)
+                        GameData.options.Volume = 0;
+                }
+            }
+            else
+            {
+                GetComponent<Image>().color = new Color(255, 255, 255, 255);
+            }
         }
 
         if(GameData.options.UseController)
         {
             if (index == 0)
                 anim.SetBool("isChoosen", true);
-            else
+            else if(!isSlider)
                 anim.SetBool("isChoosen", false);
         }
         else
         {
             if (index == 1)
                 anim.SetBool("isChoosen", true);
-            else
+            else if(!isSlider)
                 anim.SetBool("isChoosen", false);
         }
+
+        GetComponent<Image>().fillAmount = GameData.options.Volume;
 
         doSomething();
     }
