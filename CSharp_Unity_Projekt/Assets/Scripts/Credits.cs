782c113ed6b82abc88d7bd8143e215d9f8426f67
MichaelWinsauer <46480126+MichaelWinsauer@users.noreply.github.com>
2019-06-26 19:10:27 +0200

    Added Credits

---
+++ CSharp_Unity_Projekt/Assets/Scripts/Credits.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+
+    private float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 65f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
