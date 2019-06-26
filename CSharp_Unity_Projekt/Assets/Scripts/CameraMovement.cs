3f8dfccfb1037c06cbef48dd3590fb4353e30cf3
MichaelWinsauer <michael@winsauer.de>
2019-06-26 10:56:26 +0200

    Control Hilfe hinzugefügt

--- CSharp_Unity_Projekt/Assets/Scripts/CameraMovement.cs
+++ CSharp_Unity_Projekt/Assets/Scripts/CameraMovement.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class CameraMovement : MonoBehaviour
 {
@@ -17,11 +19,48 @@ public class CameraMovement : MonoBehaviour
 
     private float speed = 0.15f;
     private Transform player;
+    private string currentScene;
+
 
     //Das Spielerobjekt wird im Projekt gesucht und es wird eine Referenz dazu gebildet.
     public void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+        currentScene = ((Scene)SceneManager.GetActiveScene()).name;
+
+        if (Screen.currentResolution.width == 3000)
+        {
+            switch(currentScene)
+            {
+                case "Level 1":
+                    minX = -28.49f;
+                    maxX = 104.5f;
+                    break;
+
+                case "Level 2":
+                    minX = -68.6f;
+                    break;
+
+                case "Level 3":
+                    minX = -66.37f;
+                    break;
+
+                case "Level 4":
+                    minX = -40.6f;
+                    maxX = 23.4f;
+                    break;
+
+                case "Level 5":
+                    minX = -68.51f;
+                    maxX = 64.5f;
+                    break;
+
+                case "Level 6":
+
+                    break;
+            }
+        }
     }
 
     // Die Kameraposition wird auf die des Spielers gesetzt. .Lerp ist eine Funktion, die langsahm von einer Position auf die nächste beschleunigt.
---
+++ CSharp_Unity_Projekt/Assets/Scripts/ControlHelp.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlHelp : MonoBehaviour
+{
+    [SerializeField]
+    private string controlName;
+    [SerializeField]
+    private bool isAxis;
+
+    private Animator anim;
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(player.transform.position.x - transform.position.x, player.transform.position.y - transform.position.y), 7f);
+        if(hit.collider != null && (hit.collider.CompareTag("Player") || hit.collider.CompareTag("MeleeTrigger")))
+        {
+            if(!anim.GetBool("isDone"))
+            {
+                anim.SetTrigger("appear");
+                if (isAxis)
+                {
+                    if (Input.GetAxis(controlName) != 0)
+                    {
+                        anim.SetTrigger("disappear");
+                        anim.SetBool("isDone", true);
+                    }
+                }
+                else
+                {
+                    if (Input.GetButtonDown(controlName))
+                    {
+                        anim.SetTrigger("disappear");
+                        anim.SetBool("isDone", true);
+                        
+                    }
+                }
+            }
+        }
+        else
+        {
+            anim.SetTrigger("disappear");
+        }
+    }
+}
